d1a6960712791e256b5857a23638435114f6ce16
Martins Ake <akemartins13@gmail.com>
2025-09-28 07:17:04 +0100

    added few update: headline text, about us dropdown view

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import { newsData } from "../newsData";
